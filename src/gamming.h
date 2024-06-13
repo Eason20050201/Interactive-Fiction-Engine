8366a9595b3e62ccbf273e00b7c29d8535a63983
Jerry <jerry.lee9453@gmail.com>
2024-06-14 02:50:29 +0800

    修改search_event

--- src/gamming.h
+++ src/gamming.h
@@ -11,7 +11,7 @@ typedef struct {
     char *event;
     char *next_event;
     char *scene;    // image name
-    char *charcter; // image name
+    char *character; // image name
     int affect_change;
     char *choice_a;
     char *choice_b;
