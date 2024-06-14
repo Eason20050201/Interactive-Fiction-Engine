a107531fd67c4dc41b0967ea278862b1797e524f
linjay1026 <jayandylin@gmail.com>
2024-06-14 16:20:41 +0800

    s

--- src/gamming.h
+++ src/gamming.h
@@ -5,4 +5,4 @@
 #include <string.h>
 #include "parser.h"
 
-int search_event( GameState *game_state );
+void search_event( GameState *game_state );
--- src/macro.h
+++ src/macro.h
@@ -6,7 +6,7 @@
 #define SCREEN_NEW_GAME 1
 #define SCREEN_CONTINUE_GAME 2
 #define SCREEN_GAME_LOOP 3
-// #define SCREEN_END -1
+#define SCREEN_END -1
 
 // Define window width and height as macros
 #define WINDOW_WIDTH 1280
